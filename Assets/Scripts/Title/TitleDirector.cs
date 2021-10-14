6f5c770159d74abfe4aacbcff46d26e8e8aacc9b
MSI\kouki <kouki200012@gmail.com>
2021-10-14 18:40:45 +0900

    コントローラー操作でゲーム終了とチャプター選択画面へ

---
+++ Assets/Scripts/Title/TitleDirector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class TitleDirector : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+            SceneManager.LoadScene("C_Select");
+        if (Input.GetButton("Fire2"))
+        {
+            //  終了
+            #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+            #else
+                Application.Quit();
+            #endif
+        }
+    }
+}
