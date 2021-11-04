3c429d0ee0d7c1f9daeb6e622c79a710e61b00e8
MSI\kouki <kouki200012@gmail.com>
2021-11-04 11:56:13 +0900

    2021/11/04　12時　カメラ、HPバー、各ステージ作成

--- Assets/Scripts/Title/TitleDirector.cs
+++ Assets/Scripts/Title/TitleDirector.cs
@@ -1,13 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 public class TitleDirector : MonoBehaviour
 {
+    float counter = 0;
+    bool button = false;
+    [SerializeField] int time = 1200;
+    [SerializeField] GameObject black;
+    void Start()
+    {
+        counter = 0;
+        button = false;
+     
+    }
     void Update()
     {
         if (Input.GetButton("Fire1"))
-            SceneManager.LoadScene("C_Select");
+        {
+            button = true;
+        }
+        if (button)
+        {
+            counter++;
+            black.GetComponent<Image>().color = new Color(0.0f, 0.0f, 0.0f, counter * 0.001f);
+            if (counter >= time)
+                SceneManager.LoadScene("StageOne");
+        }
         if (Input.GetButton("Fire2"))
         {
             //  終了
